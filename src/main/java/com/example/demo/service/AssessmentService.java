e15aaf94bfeceb1201babecab9b28facca04d0b3
bala-amypo <bala@amypo.in>
2025-12-20 11:30:39 +0000

    User commit

--- src/main/java/com/example/demo/service/AssessmentService.java
+++ src/main/java/com/example/demo/service/AssessmentService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import com.example.demo.entity.AssessmentResult;
 import java.util.List;
 
-public interface AssessmentResultService {
+public interface AssessmentService {
     AssessmentResult create(AssessmentResult result);
     List<AssessmentResult> getByStudent(Long studentProfileId);
     List<AssessmentResult> getByStudentAndSkill(Long studentProfileId, Long skillId);
--- src/main/java/com/example/demo/serviceimpl/AssessmentServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/AssessmentServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AssessmentResultServiceImpl implements AssessmentResultService {
+public class AssessmentServiceImpl implements AssessmentResultService {
 
     private final AssessmentResultRepository repo;
 
