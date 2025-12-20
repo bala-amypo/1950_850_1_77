5cbca391c845b67ebf9d1fb39eb6c6c1cf8463a6
bala-amypo <bala@amypo.in>
2025-12-20 11:44:07 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/AssessmentServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/AssessmentServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AssessmentServiceImpl implements AssessmentResultService {
+public class AssessmentServiceImpl implements AssessmentService {
 
     private final AssessmentResultRepository repo;
 
