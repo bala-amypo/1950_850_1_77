f3a6ce1ec752650413705c36ba3165b0f51b1c3c
bala-amypo <bala@amypo.in>
2025-12-26 06:33:01 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.serviceimpl;
 
-import com.example.demo.entity.Role;
 import com.example.demo.entity.User;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
@@ -10,39 +9,39 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AuthServiceImpl implements AuthService {
+public class UserServiceImpl implements AuthService {
 
-    private final UserRepository repo;
+    private final UserRepository userRepository;
 
-    public AuthServiceImpl(UserRepository repo) {
-        this.repo = repo;
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public User register(User user) {
-        if (repo.existsByEmail(user.getEmail())) {
+        if (userRepository.existsByEmail(user.getEmail())) {
             throw new IllegalArgumentException("Email already exists");
         }
-        return repo.save(user);
+        return userRepository.save(user);
     }
 
     @Override
     public User getById(Long id) {
-        return repo.findById(id)
+        return userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
     public User findByEmail(String email) {
-        return repo.findByEmail(email)
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
     public List<User> listInstructors() {
-        return repo.findAll()
+        return userRepository.findAll()
                 .stream()
-                .filter(u -> u.getRole() == Role.INSTRUCTOR)
+                .filter(u -> u.getRole().name().equals("INSTRUCTOR"))
                 .toList();
     }
 }
