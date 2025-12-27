fa33552bca6a43482b5d892db2c6eb20498ab666
bala-amypo <bala@amypo.in>
2025-12-27 04:42:26 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentProfileService.java
+++ src/main/java/com/example/demo/service/StudentProfileService.java
@@ -1,17 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.StudentProfile;
+
 import java.util.List;
 
 public interface StudentProfileService {
 
-    StudentProfile create(StudentProfile profile);
-
-    StudentProfile update(Long id, StudentProfile profile);
+    StudentProfile createProfile(StudentProfile profile);
 
     StudentProfile getById(Long id);
 
     List<StudentProfile> getAll();
-
-    void delete(Long id);
 }
--- src/main/java/com/example/demo/serviceimpl/StudentProfileServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/StudentProfileServiceImpl.java
@@ -2,17 +2,31 @@ package com.example.demo.serviceimpl;
 
 import com.example.demo.entity.StudentProfile;
 import com.example.demo.repository.StudentProfileRepository;
+import com.example.demo.service.StudentProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class StudentProfileServiceImpl {
+public class StudentProfileServiceImpl implements StudentProfileService {
 
     private final StudentProfileRepository repository;
 
-    public StudentProfile createOrUpdateProfile(StudentProfile profile) {
-        profile.setLastUpdatedAt(System.currentTimeMillis());
+    @Override
+    public StudentProfile createProfile(StudentProfile profile) {
         return repository.save(profile);
+    }
+
+    @Override
+    public StudentProfile getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("StudentProfile not found"));
+    }
+
+    @Override
+    public List<StudentProfile> getAll() {
+        return repository.findAll();
     }
 }
