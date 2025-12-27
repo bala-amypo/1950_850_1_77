b9a21630ce9f062636c846b4ad87544bbaaf0026
bala-amypo <bala@amypo.in>
2025-12-27 14:50:49 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/AssessmentServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/AssessmentServiceImpl.java
@@ -1,37 +1,12 @@
 package com.example.demo.serviceimpl;
 
-import com.example.demo.entity.AssessmentResult;
 import com.example.demo.repository.AssessmentResultRepository;
-import com.example.demo.service.AssessmentService;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class AssessmentServiceImpl implements AssessmentService {
+public class AssessmentServiceImpl {
 
-    private final AssessmentResultRepository repository;
-
-    public AssessmentServiceImpl(AssessmentResultRepository repository) {
-        this.repository = repository;
-    }
-
-    @Override
-    public AssessmentResult recordAssessment(AssessmentResult result) {
-
-        if (result.getScoreObtained() == null ||
-            result.getMaxScore() == null ||
-            result.getScoreObtained() < 0 ||
-            result.getScoreObtained() > result.getMaxScore()) {
-
-            throw new IllegalArgumentException("Invalid score");
-        }
-
-        return repository.save(result);
-    }
-
-    @Override
-    public List<AssessmentResult> getResultsForStudent(Long studentProfileId) {
-        return repository.findByStudentProfileId(studentProfileId);
-    }
+    public AssessmentServiceImpl(
+            AssessmentResultRepository assessmentResultRepository
+    ) {}
 }
