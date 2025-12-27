a187d93ec8cc971765c3ed9e63f038662e09a97d
bala-amypo <bala@amypo.in>
2025-12-27 18:17:02 +0000

    User commit

--- src/main/java/com/example/demo/service/RecommendationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.SkillGapRecommendation;
-import java.util.List;
-
-public interface RecommendationService {
-
-    List<SkillGapRecommendation> getRecommendations(Long studentProfileId);
-}
--- src/main/java/com/example/demo/service/SkillGapService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.SkillGapRecord;
-import java.util.List;
-
-public interface SkillGapService {
-
-    List<SkillGapRecord> calculateSkillGaps(Long studentId);
-}
