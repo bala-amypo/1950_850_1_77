f8aa3fe05ad39db0a9228673f2e66c9ec67c7136
bala-amypo <bala@amypo.in>
2025-12-27 15:14:47 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/RecommendationServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/RecommendationServiceImpl.java
@@ -1,17 +1,34 @@
 package com.example.demo.serviceimpl;
 
-import com.example.demo.repository.*;
+import com.example.demo.repository.AssessmentResultRepository;
+import com.example.demo.repository.SkillGapRecommendationRepository;
+import com.example.demo.repository.SkillRepository;
+import com.example.demo.repository.StudentProfileRepository;
+import com.example.demo.service.RecommendationService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RecommendationServiceImpl {
+public class RecommendationServiceImpl implements RecommendationService {
+
+    private final AssessmentResultRepository assessmentResultRepository;
+    private final SkillGapRecommendationRepository recommendationRepository;
+    private final StudentProfileRepository studentProfileRepository;
+    private final SkillRepository skillRepository;
 
     public RecommendationServiceImpl(
             AssessmentResultRepository assessmentResultRepository,
             SkillGapRecommendationRepository recommendationRepository,
             StudentProfileRepository studentProfileRepository,
             SkillRepository skillRepository
-    ) {}
+    ) {
+        this.assessmentResultRepository = assessmentResultRepository;
+        this.recommendationRepository = recommendationRepository;
+        this.studentProfileRepository = studentProfileRepository;
+        this.skillRepository = skillRepository;
+    }
 
-    public void computeRecommendationsForStudent(Long studentId) {}
+    @Override
+    public void computeRecommendationsForStudent(Long studentId) {
+       
+    }
 }
